281a9c0180a70be11067b1ae5bda6ee1a7b4da03
Chad Carbert <chadcarbert@me.com>
2015-12-25 22:47:26 -0700

    Initial Commit

---
+++ Parklandia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parklandia
+//
+//  Created by Chad Carbert on 2015-12-25.
+//  Copyright © 2015 Maple + Pine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
